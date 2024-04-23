cfc7181f7bc5055d14e7daf3854938d09bea7231
Saqlain Sultan <saqlainsultan991@gmail.com>
2024-04-23 11:21:17 +0530

    time & date fixed.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   const now = new Date();
 
   const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
